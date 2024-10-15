7043c532b19291e5c331f6a662a74e7d2dffb980
Mohamed Elbehary <mobehairy7@gmail.com>
2024-10-15 04:06:16 +0300

    15/10/2024

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,6 +148,21 @@ $('.services-carousel.owl-carousel').owlCarousel({
     }
 });
 
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+          $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+          $('#imagePreview').hide();
+          $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#imageUpload").change(function() {
+  readURL(this);
+});
+
 $('.domestic-carousel .owl-carousel').owlCarousel({
     loop: true,
     margin: 20,
